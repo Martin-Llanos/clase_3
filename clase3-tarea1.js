c98be633f1f1ef429bfff369df97dd33201c808e
Martin-Llanos <112277462+Martin-Llanos@users.noreply.github.com>
2023-08-18 17:19:31 -0300

    Update clase3-tarea1.js

--- clase3-tarea1.js
+++ clase3-tarea1.js
@@ -2,10 +2,10 @@ const nombreDeUsuario = prompt("Como te llamas?" || '').toLowerCase();
 const miNombre = 'Miguel'
 const nombreDeMiHermano = 'Luis'
 
-if (nombreDeUsuario === 'Miguel'){
-    console.log("Hola, Tocayo! Yo también me llamo " + miNombre);
-} else if (nombreDeUsuario === 'luis'){
-   console.log("Hola " + nombreDeMiHermano + " , te llamás igual que mi hermano");
+if (nombreDeUsuario === 'sebastian'){
+    console.log("Hola, Tocayo! Yo también me llamo " + Miguel);
+} else if (nombreDeUsuario === 'luciano'){
+   console.log("Hola " + Luis + " , te llamás igual que mi hermano");
 } else {
-    console.log("Hola, " + nombreDeUsuario)
+    console.log("Hola, " + Miguel)
 }
