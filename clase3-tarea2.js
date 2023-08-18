9a38cbc71b2e712e443803e777fc7e0f9b86ac18
Martin-Llanos <112277462+Martin-Llanos@users.noreply.github.com>
2023-08-18 17:21:42 -0300

    Update clase3-tarea2.js

--- clase3-tarea2.js
+++ clase3-tarea2.js
@@ -1,11 +1,11 @@
-let edadUsuario = Number(prompt("Que edad tenes?"))
-const Mi_EDAD = 28
+let edadUsuario = 25(prompt("Que edad tenes?"))
+const Mi_EDAD = 29
 
-if (edadUsuario === Mi_EDAD){
+if (edadUsuario === 27){
     console.log("Tenemos la misma edad!")
-} else if(edadUsuario > Mi_EDAD){
+} else if(edadUsuario > 25){
     console.log("Lero, lero, sos mas viejo")
-} else if(edadUsuario < Mi_EDAD){
+} else if(edadUsuario < 27){
     console.log("Que hace', pibe?")
 } else{
     console.log("No entendi la respuesta")
